3571420d91cc407f19095a72367bbf79575e9326
Ashwin Sunuwar <ashwinsunuwarr2@gmail.com>
2024-02-29 14:42:19 +0545

    social icons insertion

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
+import {
+  PiInstagramLogoLight,
+  PiLinkedinLogo,
+  PiGithubLogoLight,
+} from "react-icons/pi";
+
+import { SiGmail } from "react-icons/si";
 function Header() {
   return (
     <header>
@@ -59,7 +66,7 @@ function Header() {
           </ul>
 
           <div>
-            <nav className="flex gap-4">
+            {/* <nav className="flex gap-4">
               <Link
                 to="/login"
                 className="bg-gray-500 text-white px-4 py-2 rounded shadow-inner shadow-white/50 hover:text-blue-400"
@@ -72,7 +79,60 @@ function Header() {
               >
                 Register
               </Link>
-            </nav>
+            </nav> */}
+
+            {/* socials-------- */}
+
+            <div className="flex gap-4">
+              <button
+                className="middle none relative h-10 max-h-[40px] w-10 max-w-[40px] rounded-lg bg-pink-500 text-center font-sans text-xs font-medium uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                type="button"
+                data-ripple-light="true"
+                onClick={() =>
+                  window.open("https://www.instagram.com/ashwinsunuwar/")
+                }
+              >
+                <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 transform">
+                  <PiInstagramLogoLight size={30} />
+                </span>
+              </button>
+              <button
+                className="middle none relative h-10 max-h-[40px] w-10 max-w-[40px] rounded-lg bg-red-500 text-center font-sans text-xs font-medium uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                type="button"
+                data-ripple-light="true"
+                onClick={() =>
+                  window.open("https://github.com/AshwinSunuwarr2")
+                }
+              >
+                <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 transform">
+                  <PiGithubLogoLight size={30} />
+                </span>
+              </button>
+              <button
+                className="middle none relative h-10 max-h-[40px] w-10 max-w-[40px] rounded-lg bg-green-500 text-center font-sans text-xs font-medium uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                type="button"
+                data-ripple-light="true"
+                onClick={() =>
+                  window.open(
+                    "https://www.linkedin.com/in/ashwin-sunuwar-648769229/"
+                  )
+                }
+              >
+                <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 transform">
+                  <PiLinkedinLogo size={30} />
+                </span>
+              </button>
+              <button
+                className="middle none relative h-10 max-h-[40px] w-10 max-w-[40px] rounded-lg bg-amber-500 text-center font-sans text-xs font-medium uppercase text-black shadow-md shadow-amber-500/20 transition-all hover:shadow-lg hover:shadow-amber-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                type="button"
+                data-ripple-light="true"
+                onClick={() => window.open("mailto:ashwinsunuwarr2@gmail.com")}
+              >
+                <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 transform">
+                  <SiGmail size={25} />
+                </span>
+              </button>
+            </div>
           </div>
         </div>
       </nav>
