06b51e3ac5b95bb18c257635808e04054e30d806
Ashwin Sunuwar <ashwinsunuwarr2@gmail.com>
2024-03-18 09:03:48 +0545

    new contacts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import Email from "emailjs-com";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import {
   PiInstagramLogoLight,
@@ -30,7 +30,7 @@ function Contact() {
     )
       .then((response) => {
         console.log("Email sent successfully:", response);
-        
+
         navigate("/");
       })
       .catch((error) => {
@@ -94,64 +94,148 @@ function Contact() {
                   GET IN TOUCH WITH ME
                 </h2>
                 <p className="text-base text-body-color leading-relaxed mb-9">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eius tempor incididunt ut labore et dolore magna aliqua. Ut
-                  enim adiqua minim veniam quis nostrud exercitation ullamco
+                  Feel free to drop me a message if you have any questions,
+                  inquiries, or just want to say hello. I'm always open to new
+                  opportunities, collaborations, and discussions. Looking
+                  forward to hearing from you soon!
                 </p>
                 {/* socials-------- */}
 
-              <div className="md:hidden sm:flex gap-3">
-                <button
-                  className="middle none relative h-10 max-h-[35px] w-10 max-w-[35px] rounded-lg bg-pink-500 text-center font-sans text-xs font-medium uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mr-2"
-                  type="button"
-                  data-ripple-light="true"
-                  onClick={() =>
-                    window.open("https://www.instagram.com/ahwinsunuwar/")
-                  }
-                >
-                  <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 transform">
-                    <PiInstagramLogoLight size={30} />
-                  </span>
-                </button>
-                <button
-                  className="middle none relative h-10 max-h-[35px] w-10 max-w-[35px] rounded-lg bg-red-500 text-center font-sans text-xs font-medium uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mr-2"
-                  type="button"
-                  data-ripple-light="true"
-                  onClick={() =>
-                    window.open("https://github.com/AshwinSunuwarr2")
-                  }
-                >
-                  <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 transform">
-                    <PiGithubLogoLight size={30} />
-                  </span>
-                </button>
-                <button
-                  className="middle none relative h-10 max-h-[35px] w-10 max-w-[35px] rounded-lg bg-green-500 text-center font-sans text-xs font-medium uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mr-2"
-                  type="button"
-                  data-ripple-light="true"
-                  onClick={() =>
-                    window.open(
-                      "https://www.linkedin.com/in/ashwin-sunuwar-648769229/"
-                    )
-                  }
-                >
-                  <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 transform">
-                    <PiLinkedinLogo size={30} />
-                  </span>
-                </button>
-                <button
-                  className="middle none relative h-10 max-h-[35px] w-10 max-w-[35px] rounded-lg bg-amber-500 text-center font-sans text-xs font-medium uppercase text-black shadow-md shadow-amber-500/20 transition-all hover:shadow-lg hover:shadow-amber-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                  type="button"
-                  data-ripple-light="true"
-                  onClick={() =>
-                    window.open("mailto:ashwinsunuwarr2@gmail.com")
-                  }
-                >
-                  <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 transform">
-                    <SiGmail size={25} />
-                  </span>
-                </button>
-              </div>
+                <div className="md:hidden sm:flex gap-3">
+                  <button
+                    className="middle none relative h-10 max-h-[35px] w-10 max-w-[35px] rounded-lg bg-pink-500 text-center font-sans text-xs font-medium uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mr-2"
+                    type="button"
+                    data-ripple-light="true"
+                    onClick={() =>
+                      window.open("https://www.instagram.com/ahwinsunuwar/")
+                    }
+                  >
+                    <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 transform">
+                      <PiInstagramLogoLight size={30} />
+                    </span>
+                  </button>
+                  <button
+                    className="middle none relative h-10 max-h-[35px] w-10 max-w-[35px] rounded-lg bg-red-500 text-center font-sans text-xs font-medium uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mr-2"
+                    type="button"
+                    data-ripple-light="true"
+                    onClick={() =>
+                      window.open("https://github.com/AshwinSunuwarr2")
+                    }
+                  >
+                    <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 transform">
+                      <PiGithubLogoLight size={30} />
+                    </span>
+                  </button>
+                  <button
+                    className="middle none relative h-10 max-h-[35px] w-10 max-w-[35px] rounded-lg bg-green-500 text-center font-sans text-xs font-medium uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mr-2"
+                    type="button"
+                    data-ripple-light="true"
+                    onClick={() =>
+                      window.open(
+                        "https://www.linkedin.com/in/ashwin-sunuwar-648769229/"
+                      )
+                    }
+                  >
+                    <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 transform">
+                      <PiLinkedinLogo size={30} />
+                    </span>
+                  </button>
+                  <button
+                    className="middle none relative h-10 max-h-[35px] w-10 max-w-[35px] rounded-lg bg-amber-500 text-center font-sans text-xs font-medium uppercase text-black shadow-md shadow-amber-500/20 transition-all hover:shadow-lg hover:shadow-amber-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    type="button"
+                    data-ripple-light="true"
+                    onClick={() =>
+                      window.open("mailto:ashwinsunuwarr2@gmail.com")
+                    }
+                  >
+                    <span className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 transform">
+                      <SiGmail size={25} />
+                    </span>
+                  </button>
+                </div>
+
+                <div className="w-full md:w-[48.5%] mt-6">
+                  <div
+                    style={{
+                      animation:
+                        "scale-up-center 1s cubic-bezier(0.4, 0, 0.2, 1) both; ",
+                    }}
+                  >
+                    {/* telegram----- */}
+                    <Link to='https://t.me/ashwinsunuwar' target="_blank">
+                    <button className="dark:bg-blue-400 w-full bg-slate-900 duration-200">
+                      <div className="bg-blue-400 dark:bg-slate-900 dark:border-blue-400 active:translate-x-0 active:translate-y-0 flex items-center border-slate-900 border-2 duration-200 px-4 py-2 -translate-x-1 -translate-y-1 hover:-translate-x-1.5 hover:-translate-y-1.5 w-full">
+                        <div className="dark:text-blue-400 mr-4 flex items-center justify-center">
+                          <div className="w-10 h-10">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="2.5em"
+                              height="2.5em"
+                              viewBox="0 0 256 256"
+                            >
+                              <defs>
+                                <linearGradient
+                                  id="logosTelegram0"
+                                  x1="50%"
+                                  x2="50%"
+                                  y1="0%"
+                                  y2="100%"
+                                >
+                                  <stop offset="0%" stop-color="#2aabee" />
+                                  <stop offset="100%" stop-color="#229ed9" />
+                                </linearGradient>
+                              </defs>
+                              <path
+                                fill="url(#logosTelegram0)"
+                                d="M128 0C94.06 0 61.48 13.494 37.5 37.49A128.038 128.038 0 0 0 0 128c0 33.934 13.5 66.514 37.5 90.51C61.48 242.506 94.06 256 128 256s66.52-13.494 90.5-37.49c24-23.996 37.5-56.576 37.5-90.51c0-33.934-13.5-66.514-37.5-90.51C194.52 13.494 161.94 0 128 0"
+                              />
+                              <path
+                                fill="#fff"
+                                d="M57.94 126.648c37.32-16.256 62.2-26.974 74.64-32.152c35.56-14.786 42.94-17.354 47.76-17.441c1.06-.017 3.42.245 4.96 1.49c1.28 1.05 1.64 2.47 1.82 3.467c.16.996.38 3.266.2 5.038c-1.92 20.24-10.26 69.356-14.5 92.026c-1.78 9.592-5.32 12.808-8.74 13.122c-7.44.684-13.08-4.912-20.28-9.63c-11.26-7.386-17.62-11.982-28.56-19.188c-12.64-8.328-4.44-12.906 2.76-20.386c1.88-1.958 34.64-31.748 35.26-34.45c.08-.338.16-1.598-.6-2.262c-.74-.666-1.84-.438-2.64-.258c-1.14.256-19.12 12.152-54 35.686c-5.1 3.508-9.72 5.218-13.88 5.128c-4.56-.098-13.36-2.584-19.9-4.708c-8-2.606-14.38-3.984-13.82-8.41c.28-2.304 3.46-4.662 9.52-7.072"
+                              />
+                            </svg>
+                          </div>
+                        </div>
+                        <h4 className="dark:text-blue-400 duration-200">
+                          <div className="flex justify-start items-center">
+                            Telegram
+                          </div>
+                        </h4>
+                      </div>
+                    </button>
+                    </Link>
+
+                    {/* whatsapp-------- */}
+
+                    <Link to='https://call.whatsapp.com/voice/21IqNBP9Iq4U8sn1MuBF2o' target="_blank">
+                    <button className="dark:bg-green-400 w-full bg-slate-900 duration-200 mt-6">
+                      <div className="bg-green-400 dark:bg-slate-900 dark:border-green-400 active:translate-x-0 active:translate-y-0 flex items-center border-slate-900 border-2 duration-200 px-4 py-2 -translate-x-1 -translate-y-1 hover:-translate-x-1.5 hover:-translate-y-1.5 w-full">
+                        <div className="dark:text-blue-400 mr-4 flex items-center justify-center">
+                          <div className="w-10 h-10">
+                          <svg width="2.5em" height="2.5em" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M16 31C23.732 31 30 24.732 30 17C30 9.26801 23.732 3 16 3C8.26801 3 2 9.26801 2 17C2 19.5109 2.661 21.8674 3.81847 23.905L2 31L9.31486 29.3038C11.3014 30.3854 13.5789 31 16 31ZM16 28.8462C22.5425 28.8462 27.8462 23.5425 27.8462 17C27.8462 10.4576 22.5425 5.15385 16 5.15385C9.45755 5.15385 4.15385 10.4576 4.15385 17C4.15385 19.5261 4.9445 21.8675 6.29184 23.7902L5.23077 27.7692L9.27993 26.7569C11.1894 28.0746 13.5046 28.8462 16 28.8462Z" fill="#BFC8D0"/>
+<path d="M28 16C28 22.6274 22.6274 28 16 28C13.4722 28 11.1269 27.2184 9.19266 25.8837L5.09091 26.9091L6.16576 22.8784C4.80092 20.9307 4 18.5589 4 16C4 9.37258 9.37258 4 16 4C22.6274 4 28 9.37258 28 16Z" fill="url(#paint0_linear_87_7264)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M16 30C23.732 30 30 23.732 30 16C30 8.26801 23.732 2 16 2C8.26801 2 2 8.26801 2 16C2 18.5109 2.661 20.8674 3.81847 22.905L2 30L9.31486 28.3038C11.3014 29.3854 13.5789 30 16 30ZM16 27.8462C22.5425 27.8462 27.8462 22.5425 27.8462 16C27.8462 9.45755 22.5425 4.15385 16 4.15385C9.45755 4.15385 4.15385 9.45755 4.15385 16C4.15385 18.5261 4.9445 20.8675 6.29184 22.7902L5.23077 26.7692L9.27993 25.7569C11.1894 27.0746 13.5046 27.8462 16 27.8462Z" fill="white"/>
+<path d="M12.5 9.49989C12.1672 8.83131 11.6565 8.8905 11.1407 8.8905C10.2188 8.8905 8.78125 9.99478 8.78125 12.05C8.78125 13.7343 9.52345 15.578 12.0244 18.3361C14.438 20.9979 17.6094 22.3748 20.2422 22.3279C22.875 22.2811 23.4167 20.0154 23.4167 19.2503C23.4167 18.9112 23.2062 18.742 23.0613 18.696C22.1641 18.2654 20.5093 17.4631 20.1328 17.3124C19.7563 17.1617 19.5597 17.3656 19.4375 17.4765C19.0961 17.8018 18.4193 18.7608 18.1875 18.9765C17.9558 19.1922 17.6103 19.083 17.4665 19.0015C16.9374 18.7892 15.5029 18.1511 14.3595 17.0426C12.9453 15.6718 12.8623 15.2001 12.5959 14.7803C12.3828 14.4444 12.5392 14.2384 12.6172 14.1483C12.9219 13.7968 13.3426 13.254 13.5313 12.9843C13.7199 12.7145 13.5702 12.305 13.4803 12.05C13.0938 10.953 12.7663 10.0347 12.5 9.49989Z" fill="white"/>
+<defs>
+<linearGradient id="paint0_linear_87_7264" x1="26.5" y1="7" x2="4" y2="28" gradientUnits="userSpaceOnUse">
+<stop stop-color="#5BD066"/>
+<stop offset="1" stop-color="#27B43E"/>
+</linearGradient>
+</defs>
+</svg>
+                          </div>
+                        </div>
+                        <h4 className="dark:text-green-400 duration-200">
+                          <div className="flex justify-start items-center">
+                            Whatsapp
+                          </div>
+                        </h4>
+                      </div>
+                    </button>
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="w-full lg:w-1/2 xl:w-5/12 px-4">
