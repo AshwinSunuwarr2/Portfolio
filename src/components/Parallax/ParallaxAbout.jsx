e9e78ae513cf55a813d326ca9446eb3671d2d263
Ashwin Sunuwar <ashwinsunuwarr2@gmail.com>
2024-03-31 14:56:10 +0545

    para img for experience

--- src/components/Parallax/ParallaxAbout.jsx
+++ src/components/Parallax/ParallaxAbout.jsx
@@ -5,7 +5,6 @@ import me from "../../assets/images/me.png";
 import "../compo.css";
 import { Link } from "react-router-dom";
 
-
 import {
   PiInstagramLogoLight,
   PiLinkedinLogo,
@@ -16,22 +15,21 @@ import { SiGmail } from "react-icons/si";
 const ParallaxAbout = () => {
   return (
     <>
-      <Parallax pages={2} className="bg-[#2f2f2f]">
+      <Parallax pages={4} className="bg-[#2f2f2f]">
         <ParallaxLayer offset={0} className="bg-[#7b7554]"></ParallaxLayer>
         <ParallaxLayer offset={0.2} speed={0.5}>
           <h1
             className="text-center text-[4rem] md:text-[8rem] 
-           font-bold font-montserrat leading-wider tracking-wider text-[#7b7554] drop-shadow-[8px_8px_5px_#00000093]"
+           font-bold font-montserrat leading-wider tracking-wider text-[#89825e] drop-shadow-[8px_8px_5px_#00000052] inset-1"
           >
             Greetings! Lorem Ipsum daily
           </h1>
         </ParallaxLayer>
 
         <ParallaxLayer offset={1}></ParallaxLayer>
-        <ParallaxLayer offset={1.1}>
+        <ParallaxLayer offset={1.2}>
           <div className="flex flex-col-reverse md:flex-row justify-between">
             <div className="flex flex-grow">
-              
               {/* socials-------- */}
 
               <div className="flex flex-row gap-5 ml-4">
@@ -93,29 +91,51 @@ const ParallaxAbout = () => {
                     className="text-start text-[1rem] md:text-[4rem] 
            font-bold font-montserrat leading-wider tracking-wider text-[#7b7554] drop-shadow-[8px_8px_5px_#00000093] ml-14"
                   >
-                    Ashwin Sunuwar
+                    Er. Ashwin Sunuwar
                   </h1>
                   <p
                     className="text-start text-[1rem] md:text-[2rem] 
            font-medium font-montserrat leading-wider tracking-wider text-[#7b7554] drop-shadow-[8px_8px_5px_#00000093] ml-14"
                   >
-                    From Sindhuli, Nepal
+                    From Sindhuli, Nepal <br />
+                    Lalitpur, current <br />
+                    Computer Engineer
+                    <br />
+                    Email -&nbsp;
+                    <Link
+                      to="mailto:ashwinsunuwarr2@gmail.com"
+                      className="underline"
+                    >
+                      ashwinsunuwarr2@gmail.com
+                    </Link>
                   </p>
                 </div>
               </ParallaxLayer>
             </div>
-          
-          <div className="relative lg:mr-10 z-[-1] ">
-            
-            <img
-              src={me}
-              alt="me"
-              width={400}
-              className="rounded-full aspect-[1/1] object-cover shadow-[0px_0px_14px_8px_#7b7554] shadow-[#00000039]"
-            />
-            <div className="absolute top-0 left-0 bg-[#0000003e] w-full h-full rounded-full "></div>
+
+            <div className="relative lg:mr-10 z-[-1] ">
+              <img
+                src={me}
+                alt="me"
+                width={400}
+                className="rounded-full aspect-[1/1] object-cover shadow-[0px_0px_14px_8px_#7b7554] shadow-[#00000039]"
+              />
+              <div className="absolute top-0 left-0 bg-[#0000003e] w-full h-full rounded-full "></div>
+            </div>
           </div>
-          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={2} factor={2} speed={0.2}>
+          <img src="https://images.unsplash.com/photo-1553877522-43269d4ea984?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="exp-bg" className="relative opacity-[0.2]"/>
+        
+        </ParallaxLayer>
+        <ParallaxLayer offset={2.2} >
+        <h1
+                    className="text-start text-[1rem] md:text-[4rem] 
+           font-bold font-montserrat leading-wider tracking-wider text-[#a29a70] drop-shadow-[8px_8px_5px_#00000093] ml-14"
+                  >
+                    Experiences
+                  </h1>
         </ParallaxLayer>
       </Parallax>
     </>
